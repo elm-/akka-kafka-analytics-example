ec1deb5bd67b9a5df0a1ece8975e2e29035d05d7
Elmar Weber <elmar.weber@cupenya.com>
2017-11-09 18:15:16 +0100

    - stats step

--- src/main/scala/org/elmarweber/github/akkakafkaexample/statistics/StatisticsStream.scala
+++ src/main/scala/org/elmarweber/github/akkakafkaexample/statistics/StatisticsStream.scala
@@ -16,30 +16,7 @@ trait StatisticsStream extends StrictLogging {
   def createStream(consumerSettings: ConsumerSettings[String, String],
                    producerSettings: ProducerSettings[String, String],
                    sourceTopic: String, targetTopic: String)(implicit fmt: Materializer) = {
-    Consumer.committableSource(consumerSettings, Subscriptions.topics(sourceTopic))
-      .map { msg =>
-        (msg.record.value().parseJson.convertTo[EnrichedAnalyticsEvent], msg.committableOffset)
-      }
-      .groupBy(1024, { case (event, _) => event.artist })
-      .groupedWithin(Integer.MAX_VALUE, 10.seconds)
-      .map { groupedData =>
-        val events = groupedData.map(_._1)
-        val offsets = groupedData.map(_._2)
-        val batchOffset = offsets.foldLeft(CommittableOffsetBatch.empty) { case (batch, offset) => batch.updated(offset) }
-        val playCount = events.size
-        val windowEnd = System.currentTimeMillis()
-        val key = s"${events.head.artist}_${windowEnd}"
-        logger.info(s"Aggregated for artist '${events.head.artist}' ${playCount} play events")
-        ProducerMessage.Message(new ProducerRecord(targetTopic, key, playCount.toString), batchOffset)
-      }
-      .mergeSubstreams
-      .via(Producer.flow(producerSettings))
-      .groupedWithin(1024, 5.seconds)
-      .mapAsync(1) { producerMessages =>
-        val latestBatchOffset = producerMessages.last.message.passThrough
-        latestBatchOffset.commitScaladsl()
-      }
-      .runWith(Sink.ignore)
+    // aggregate per artist each 10 seconds how many plays it has
   }
 }
 
