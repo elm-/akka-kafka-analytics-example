f149cf56e63f7416e2d219b66a9aa5694f33e75e
Elmar Weber <elmar.weber@cupenya.com>
2017-11-09 17:40:02 +0100

    - added some simple values

--- src/main/scala/org/elmarweber/github/akkakafkaexample/mobileclient/MobileClientSim.scala
+++ src/main/scala/org/elmarweber/github/akkakafkaexample/mobileclient/MobileClientSim.scala
@@ -7,8 +7,10 @@ import akka.http.scaladsl.client.RequestBuilding
 import akka.stream.ActorMaterializer
 import org.elmarweber.github.akkakafkaexample.lib.{AnalyticsEvent, HttpClient}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.util.Random
 
 /**
  * Generates some random events and sends them to the server.
@@ -25,15 +27,22 @@ object MobileClientSim extends App {
     val event = createEvent()
     val request = RequestBuilding.Post("/api/events", event)
     Await.result(gatewayClient.doCheckedRest(request), 1.second)
-    Thread.sleep(1000)
+    Thread.sleep(50)
   }
 
   def createEvent() = {
     AnalyticsEvent(
       id = UUID.randomUUID().toString(),
-      clientId = "elmar",
+      clientId = Random.shuffle(ClientIds).head,
       songTitle = "test",
       ip = "127.0.0.1"
     )
   }
+
+  def ClientIds = List(
+    "elmar",
+    "john",
+    "jack",
+    "mark"
+  )
 }
